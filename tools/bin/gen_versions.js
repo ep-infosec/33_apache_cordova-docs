5aa1f423557fd99beb0d43c6af93288669051f1c
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-10 20:39:05 +0200

    init

---
+++ tools/bin/gen_versions.js
@@ -0,0 +1,83 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+'use strict';
+
+var path = require('path');
+var yaml = require('js-yaml');
+const semver = require('semver');
+var util = require('./util');
+
+// constants
+var LANGUAGE_MAP = {
+    en: 'English'
+};
+
+function main () {
+    var scriptName = process.argv[1];
+    var rootDir = process.argv[2];
+    var config = {};
+
+    if (!rootDir) {
+        console.error('Please specify a directory from which to generate.');
+        process.exit(1);
+    }
+
+    // go through directory that contains all languages
+    util.listdirsSync(rootDir).forEach(function (langId) {
+        var langPath = path.join(rootDir, langId);
+        var versionNames = util.listdirsSync(langPath);
+
+        // Remove dev version for semver sort. We'll add it back later.
+        versionNames.splice(versionNames.indexOf('dev'), 1);
+
+        // semver doesn't like a value of 10.x, so we'll coerce the values into proper 10.0.0,
+        // and store a map to easily convert map our sorted away back to our desired text.
+        const coercionMap = {};
+        versionNames = versionNames.map((v) => {
+            const coerced = semver.coerce(v).toString();
+            coercionMap[coerced] = v;
+            return coerced;
+        });
+
+        versionNames = semver.sort(versionNames);
+
+        // Now we can restore our desired labelling
+        versionNames = versionNames.map((v) => coercionMap[v]);
+
+        // Finally, don't forget to restore our dev version
+        versionNames.push('dev');
+
+        // get language ID
+        var langName = LANGUAGE_MAP[langId];
+        if (!langName) {
+            console.error("Language identifier '" + langId + "' doesn't have an associated name. Please fix that by changing " + scriptName + '.');
+            process.exit(1);
+        }
+
+        // set the language name and the versions it has
+        config[langId] = {
+            name: langName,
+            versions: versionNames
+        };
+    });
+
+    console.log(util.generatedBy(__filename));
+    console.log(yaml.dump(config, { indent: 4 }));
+}
+
+main();
---
+++ www/static/js/index.js
@@ -0,0 +1,159 @@
+---
+---
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+function copyToClipboard(evt) {
+    let text = evt.querySelector('.text-to-copy').textContent;
+    navigator.clipboard.writeText(text);
+}
+
+function openNpmjsPluginsSearch() {
+    window.open('https://www.npmjs.com/search?q=keywords:ecosystem:cordova', '_blank');
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+var lastVisit = getCookie("visitTime");
+function checkNotification() {
+    var dates = [];
+    if (lastVisit != "") {
+        {% for post in site.posts %}
+        dates.push('{{ post.date | date_to_rfc822 }}');{% endfor %}
+    }
+    var new_blog_count = 0;
+    for(var i = 0; i < dates.length ; i++) {
+        var blog_time = new Date(dates[i]).getTime();
+        if(blog_time > lastVisit) {
+            new_blog_count++;
+        }
+        else {
+            break;
+        }
+    }
+    return new_blog_count;
+}
+
+$(document).ready(function () {
+
+    // code for blog badge
+    $('.adorner').each(function(i) {
+        var blog_time = new Date($(this).attr('blogTime')).getTime();
+        if(lastVisit != "" && blog_time > lastVisit) {
+            this.style.backgroundColor = "#3992ab";
+        }
+    });
+
+    var new_blog_count = checkNotification();
+    if (new_blog_count) {
+        document.getElementById("new_blog_count").innerHTML = new_blog_count;
+    }
+
+    var copyButtons = document.getElementsByClassName("btn-copy");
+    for (var i = 0; i < copyButtons.length; i++) {
+        copyButtons[i].addEventListener("click", function (clickEvent) {
+            var id = clickEvent.target.getAttribute("data-clipboard-target");
+
+            var range = document.createRange();
+            range.selectNode(document.getElementById(id));
+
+            var select = window.getSelection();
+            select.removeAllRanges();
+            select.addRange(range);
+
+            try {
+                document.execCommand("copy");
+            } catch (e) {
+                // Silently fail for now
+            }
+
+            select.removeAllRanges();
+        });
+    }
+
+    // Smooth scroll to anchor links
+    $("a[href^='#']").on('click', function(e) {
+
+        // scroll only if there is a hash in the link's href
+        var hash = this.hash;
+        if (hash) {
+
+            // prevent default anchor click behavior
+            e.preventDefault();
+
+            // get the fragment without the "#" symbol because location.hash
+            // is returned with it
+            var targetName = hash.slice(1);
+
+            // escape single quotes in target name because
+            // we use them in the selector to find matching targets
+            targetName.replace(/'/g, "\\\'").replace(/"/g, "\\\"");
+
+            // check if the target exists by looking at either ID or name
+            // NOTE:
+            //      we're not using "# + targetName" to select by ID
+            //      because the ID might contain special characters that
+            //      are annoying to escape
+            var targetSelector = "*[id='" + targetName + "'], *[name='" + targetName + "']";
+
+            var matchingTargets = $(targetSelector);
+            if (matchingTargets.length < 1) {
+                return;
+            }
+            if (matchingTargets.length > 1) {
+                console.warn("found more than one anchor to go to; will go to the first one");
+            }
+
+            // get resulting scroll height
+            var matchingTarget = matchingTargets.first();
+            var scrollHeight   = matchingTarget.offset().top;
+
+            // add an extra offset (to account for the fixed page header),
+            // but only if there is no "fragment-anchor" class (because it
+            // already has an offset of its own for this purpose)
+            if (!matchingTarget.hasClass("fragment-anchor")) {
+                scrollHeight -= 50;
+            }
+
+            // animate
+            $('html, body').animate(
+                {scrollTop: scrollHeight},
+                300,
+                function () {
+                    // when done, add hash to url (default click behaviour)
+                    window.location.hash = hash;
+                }
+            );
+        }
+    });
+});
